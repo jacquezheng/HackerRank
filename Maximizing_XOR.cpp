3ff712f3de36ba5c36dcac95a7af695de4ee2ea5
jacquezheng <hzheng19@illinois.edu>
2014-09-16 00:36:35 -0500

    Update Maximizing_XOR.cpp

--- Maximizing_XOR.cpp
+++ Maximizing_XOR.cpp
@@ -1,20 +1,4 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <cstdlib>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 /*
  * Complete the function below.
